3cc00b0d814e24a55dc94e65be839c5649d7baa5
Scott <yeunghl@whu.edu.cn>
2018-10-26 20:12:54 +0800

    增加异常装饰器以提示异常 完成变量 循环语句 选择语句 赋值语句的分析

--- parser.py
+++ parser.py
@@ -2,6 +2,10 @@ from lexer import *
 
 tokens_index = 0
 
+class ParseException(Exception):
+    def __init__(self,mes):
+        Exception.__init__(self,mes)
+
 class TreeNode:
     indent = 0
     def __init__(self,type):
@@ -25,106 +29,166 @@ class TreeNode:
                 c.print_tree()
             TreeNode.indent-=1
 
+def except_process(func):
+    def f(*args,**kwargs):
+        try:
+            return func(*args,**kwargs)
+        except ParseException as e:
+            print(e)
+            exit(-1)
+    return f
 
 def get_next_token():
     if tokens_index<len(tokens):
         return tokens[tokens_index]
+    else:
+        raise ParseException('lack of token at the end of code')
+
+
+def skip_next_token():
+    global tokens_index
+    tokens_index+=1
 
 def check_next_token(token_name):
     global tokens_index
+    if tokens_index>=len(tokens):
+        raise ParseException('lack of token %s at the end of code'%token_name)
     if tokens[tokens_index].name==token_name:
         tokens_index+=1
         node = TreeNode(token_name)
         node.child=None
         return node
     else:
-        raise Exception('line %d: position %d unpected token'%tokens[tokens_index].pos)
+        raise ParseException('line %d: unpected token,should be %s'%(tokens[tokens_index].pos[0],token_name))
 
+@except_process
 def parse_stmt():
-    try:
-        token = get_next_token()
-        if token.name=='if':
-            return parse_if_stmt()
-        elif token.name=='while':
-            return parse_while_stmt()
-        elif token.name=='in':
-            return parse_in_stmt()
-        elif token.name=='out':
-            return parse_out_stmt()
-        elif token.name in ['int','float','string']:
-            return parse_decl_stmt()
-        elif token.name=='{':
-            return parse_block()
-        elif token.type=='ID':
-            return parse_assign_stmt()
-        else:
-            print("line %d: position %d unpected token"%token.pos)
-            exit(-1)
-    except Exception as e:
-        print(e)
+    """
+    解析各种语句以及语句块
+    :return:
+    """
+    token = get_next_token()
+    if token.name=='if':
+        return parse_if_stmt()
+    elif token.name=='while':
+        return parse_while_stmt()
+    elif token.name=='in':
+        return parse_in_stmt()
+    elif token.name=='out':
+        return parse_out_stmt()
+    elif token.name in ['int','float','string']:
+        return parse_decl_stmt()
+    elif token.name=='{':
+        return parse_block()
+    elif token.type=='ID':
+        return parse_assign_stmt()
+    else:
+        raise ParseException("line %d: position %d unpected token"%token.pos)
 
-
+@except_process
 def parse_if_stmt():
-    pass
+    node = TreeNode('IFSTMT')
+    node.add_child(check_next_token('if'))
+    node.add_child(check_next_token('('))
+    node.add_child(check_next_token(parse_condition()))
+    node.add_child(check_next_token(')'))
+    node.add_child(parse_stmt())
+    if get_next_token().name=='else':
+        node.add_child('else')
+        node.add_child(parse_stmt())
+    return node
 
+
+@except_process
 def parse_while_stmt():
-    pass
+    node = TreeNode('WHILESTMT')
+    node.add_child(check_next_token('('))
+    node.add_child(check_next_token(parse_condition()))
+    node.add_child(check_next_token(')'))
+    node.add_child(parse_stmt())
 
+
+@except_process
 def parse_in_stmt():
-    try:
-        new_node = TreeNode('INSTMT')
-        new_node.add_child(check_next_token('in'))
-        new_node.add_child(check_next_token('('))
-        new_node.add_child(parse_id())
-        new_node.add_child(check_next_token(')'))
-        new_node.add_child(check_next_token(';'))
-        return new_node
-    except Exception as e:
-        print(e)
+    new_node = TreeNode('INSTMT')
+    new_node.add_child(check_next_token('in'))
+    new_node.add_child(check_next_token('('))
+    new_node.add_child(parse_var())
+    new_node.add_child(check_next_token(')'))
+    new_node.add_child(check_next_token(';'))
+    return new_node
 
+
+@except_process
 def parse_out_stmt():
-    try:
-        new_node = TreeNode('OUTSTMT')
-        new_node.add_child(check_next_token('out'))
-        new_node.add_child(check_next_token('('))
+    new_node = TreeNode('OUTSTMT')
+    new_node.add_child(check_next_token('out'))
+    new_node.add_child(check_next_token('('))
 
-        token = get_next_token()
-        if token.type=='ID':
-            new_node.add_child(parse_id())
-        elif token.type=='STR':
-            new_node.add_child(check_next_token(token.name))
-        else:
-            new_node.add_child(parse_expr())
-
-        new_node.add_child(check_next_token(')'))
-        new_node.add_child(check_next_token(';'))
-        return new_node
-    except Exception as e:
-        print(e)
-
+    token = get_next_token()
+    if token.type=='ID':
+        new_node.add_child(parse_var())
+    elif token.type=='STR':
+        new_node.add_child(check_next_token(token.name))
+    else:
+        new_node.add_child(parse_expr())
+    new_node.add_child(check_next_token(')'))
+    new_node.add_child(check_next_token(';'))
+    return new_node
 
 def parse_decl_stmt():
-    pass
+    cur_token = get_next_token()
+    skip_next_token()
 
+
+@except_process
 def parse_block():
     new_node = TreeNode('BLOCK')
-    try:
-        new_node.add_child(check_next_token('{'))
-        while get_next_token().name!='}':
-            new_node.add_child(parse_stmt())
-        new_node.add_child(check_next_token('}'))
-        return new_node
-    except Exception as e:
-        print(e)
+    new_node.add_child(check_next_token('{'))
+    while get_next_token().name!='}':
+        new_node.add_child(parse_stmt())
+    new_node.add_child(check_next_token('}'))
+    return new_node
 
+
+@except_process
 def parse_assign_stmt():
-    pass
+    node = TreeNode('ASSIGNSTMT')
+    node.add_child(parse_var())
+    node.add_child(check_next_token('='))
+    token = get_next_token()
+    if token.type=='STR':
+        node.add_child(check_next_token(token.name))
+    else:
+        node.add_child(parse_expr())
+    node.add_child(check_next_token(';'))
+    return node
 
-def parse_id():
-    pass
 
 def parse_expr():
     pass
+
+
+def parse_condition():
+    pass
+
+
+def parse_var():
+    token = get_next_token()
+    node = TreeNode('VAR')
+    if token.type=='ID':
+        node.add_child(check_next_token(token.name))
+        next_token = get_next_token()
+        if next_token.name=='[':
+            node.add_child(check_next_token('['))
+            node.add_child(parse_expr())
+            node.add_child(check_next_token(']'))
+        return node
+    else:
+        raise ParseException('line %d: token should be a identifier'%token.pos[0])
+
+
+
 
 if __name__ == '__main__':
     filename = input('please input the cmm file name\n')
