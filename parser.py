b6b8bf828ff0707b603699964fe466de8f4d7ab7
Scott <yeunghl@whu.edu.cn>
2018-10-27 19:04:29 +0800

    基本完成语法分析部分

--- parser.py
+++ parser.py
@@ -85,12 +85,12 @@ def parse_stmt():
     else:
         raise ParseException("line %d: position %d unpected token"%token.pos)
 
-@except_process
+
 def parse_if_stmt():
     node = TreeNode('IFSTMT')
     node.add_child(check_next_token('if'))
     node.add_child(check_next_token('('))
-    node.add_child(check_next_token(parse_condition()))
+    node.add_child(parse_condition())
     node.add_child(check_next_token(')'))
     node.add_child(parse_stmt())
     if get_next_token().name=='else':
@@ -102,10 +102,12 @@ def parse_if_stmt():
 @except_process
 def parse_while_stmt():
     node = TreeNode('WHILESTMT')
+    node.add_child(check_next_token('while'))
     node.add_child(check_next_token('('))
-    node.add_child(check_next_token(parse_condition()))
+    node.add_child(parse_condition())
     node.add_child(check_next_token(')'))
     node.add_child(parse_stmt())
+    return node
 
 
 @except_process
@@ -126,30 +128,32 @@ def parse_out_stmt():
     new_node.add_child(check_next_token('('))
 
     token = get_next_token()
-    if token.type=='ID':
-        new_node.add_child(parse_var())
-    elif token.type=='STR':
-        new_node.add_child(check_next_token(token.name))
+    if token.type=='STR':
+        new_node.add_child(parse_str())
     else:
         new_node.add_child(parse_expr())
     new_node.add_child(check_next_token(')'))
     new_node.add_child(check_next_token(';'))
     return new_node
 
+
 @except_process
 def parse_decl_stmt():
-    id_token = get_next_token()
+    type_token = get_next_token()
     decl_node = TreeNode('VARDECL')
     type_node = TreeNode('TYPE')
     decl_node.add_child(type_node)
-    type_node.add_child(check_next_token(id_token.name))
+    type_node.add_child(check_next_token(type_token.name))
     next_token = get_next_token()
     if next_token.name=='[':
         type_node.add_child(check_next_token('['))
         num_token = get_next_token()
         if num_token.type!='NUM':
             raise ParseException('Line %d: length of array should be a constant number'%num_token.pos[0])
-        type_node.add_child(check_next_token(num_token.name))
+        num_node = TreeNode('NUM')
+        num_node.value = num_token.name
+        type_node.add_child(num_node)
+        skip_next_token()
         type_node.add_child(check_next_token(']'))
     decl_node.add_child(parse_id())
     while get_next_token().name==',':
@@ -176,7 +180,7 @@ def parse_assign_stmt():
     node.add_child(check_next_token('='))
     token = get_next_token()
     if token.type=='STR':
-        node.add_child(check_next_token(token.name))
+        node.add_child(parse_str())
     else:
         node.add_child(parse_expr())
     node.add_child(check_next_token(';'))
@@ -184,7 +188,40 @@ def parse_assign_stmt():
 
 
 def parse_expr():
-    pass
+    node = TreeNode('EXPR')
+    if get_next_token().name=='-':
+        node.add_child(check_next_token('-'))
+        node.add_child(parse_expr())
+    else:
+        node.add_child(parse_term())
+        while get_next_token().name in ['+','-']:
+            node.add_child(check_next_token(get_next_token().name))
+            node.add_child(parse_term())
+    return node
+
+def parse_term():
+    node = TreeNode('TERM')
+    node.add_child(parse_factor())
+    while get_next_token().name in ['*', '/']:
+        node.add_child(check_next_token(get_next_token().name))
+        node.add_child(parse_factor())
+    return node
+
+def parse_factor():
+    node = TreeNode('FACTOR')
+    token = get_next_token()
+    if token.type=='NUM':
+        rnode = TreeNode('NUM')
+        rnode.value = token.name
+        node.add_child(rnode)
+        skip_next_token()
+    elif token.name=='(':
+        node.add_child(check_next_token('('))
+        node.add_child(parse_expr())
+        node.add_child(check_next_token(')'))
+    else:
+        node.add_child(parse_var())
+    return node
 
 
 def parse_condition():
@@ -196,18 +233,19 @@ def parse_condition():
         condition_node.add_child(check_next_token(')'))
     else:
         condition_node.add_child(parse_cond())
-        if get_next_token().name in ['&&','||']:
-            condition_node.add_child(parse_other_condition())
+        while get_next_token().name in ['&&','||']:
+            condition_node.add_child(check_next_token(get_next_token().name))
+            condition_node.add_child(parse_cond())
     return condition_node
 
 
-def parse_other_condition():
-    other_condition = TreeNode('OTHERCONDITION')
-    other_condition.add_child(check_next_token(get_next_token().name))
-    other_condition.add_child(parse_cond())
-    if get_next_token().name in ['&&', '||']:
-        other_condition.add_child(parse_other_condition())
-    return other_condition
+# def parse_other_condition():
+#     other_condition = TreeNode('OTHERCONDITION')
+#     other_condition.add_child(check_next_token(get_next_token().name))
+#     other_condition.add_child(parse_cond())
+#     if get_next_token().name in ['&&', '||']:
+#         other_condition.add_child(parse_other_condition())
+#     return other_condition
 
 
 
@@ -215,7 +253,7 @@ def parse_other_condition():
 def parse_compare():
     token = get_next_token()
     if token.name in ['<','<=','==','<>']:
-        return TreeNode(token.name)
+        return check_next_token(token.name)
     else:
         raise ParseException('Line %d: token should be a compare operand'%token.pos[0])
 
@@ -239,7 +277,7 @@ def parse_var():
     token = get_next_token()
     node = TreeNode('VAR')
     if token.type=='ID':
-        node.add_child(check_next_token(token.name))
+        node.add_child(parse_id())
         next_token = get_next_token()
         if next_token.name=='[':
             node.add_child(check_next_token('['))
@@ -255,17 +293,35 @@ def parse_id():
     if id_token.type != 'ID':
         raise ParseException('Line %d: token should be a identifier' % id_token.pos[0])
     skip_next_token()
-    return TreeNode(id_token.name)
+    rnode = TreeNode('ID')
+    rnode.value = id_token.name
+    return rnode
 
-@except_process
-def parse_cond():
-    pass
+
+def parse_str():
+    if get_next_token().type=='STR':
+        rnode = TreeNode('STR')
+        rnode.value = get_next_token().name
+        skip_next_token()
+        return rnode
+    else:
+        raise ParseException('Line %d: token should be a string '%get_next_token().pos[0])
+
+
+def parser(filename):
+    lexer(filename)
+    tokens.append(Token('END','END',None))
+    roots = TreeNode('root')
+    while tokens_index < len(tokens)-1:
+        roots.add_child(parse_stmt())
+    tokens.pop(-1)
+    return roots
+
+
+
 
 if __name__ == '__main__':
     filename = input('please input the cmm file name\n')
-    lexer(filename)
-    roots=TreeNode('root')
-    while tokens_index < len(tokens):
-        roots.add_child(parse_stmt())
+    roots=parser(filename)
     roots.print_tree()
 
